c41026c123ad3d5afd146e7b89b4fb19c227ec57
Shreyas Sriram <shreyassriram3@gmail.com>
2023-02-02 00:50:48 -0800

    yellowbelt

---
+++ buttonclicker/script.js
@@ -0,0 +1,16 @@
+// script for dojonary
+
+function removeDefinition(id){
+     var element = document.querySelector(id);
+     element.remove();
+}
+
+function ninjaLike(id){
+     alert("ninja was liked");
+     console.log("ninja was liked");
+}
+
+function logout(id) {
+     // if span text currently reads Login, change to Logout
+     // if span text currently reads Logout, change to Login
+}
---
+++ likes/script.js
@@ -0,0 +1,5 @@
+
+
+function likeButton(){
+     // increase likes by 1 for each respective person
+}
